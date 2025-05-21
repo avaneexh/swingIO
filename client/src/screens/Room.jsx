f3225880eabcc58198ca52d346c8171b80a083cf
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-05-21 19:11:59 +0530

    feat: update package dependencies and enhance Lobby and Room components with React Player integration

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,27 +1,39 @@
 import React,{ useEffect, useCallback, useState }  from 'react'
+import ReactPlayer from 'react-player'
 import { useSocket } from '../context/SocketProvider'
 
 const RoomPage = () => {
 
   const socket = useSocket();
-  const [remoteSocketId, setRemoteSocketId] = useState("");
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
 
 
-  const handleuserJoined = useCallback((email, id) => {
+
+  const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined room`);
     setRemoteSocketId(id);
-  }, [])
+  }, []);
+
+  const handleCallUser = useCallback( async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ 
+      audio:true, 
+      video:true
+    })
+    setMyStream(stream);
+  },[])
 
 
   useEffect(() => {
 
-    socket.on('user:joined', handleuserJoined)
+     socket.on("user:joined", handleUserJoined);
 
     return() => {
-      socket.off('user:joined', handleuserJoined )
+      socket.off("user:joined", handleUserJoined );
     }
 
-  }, [ socket, handleuserJoined ])
+  }, [  socket,
+    handleUserJoined, ])
 
 
   return (
@@ -32,6 +44,24 @@ const RoomPage = () => {
     <h4>
       {remoteSocketId ? 'connected' : 'No user Connected'}
     </h4>
+   {
+    remoteSocketId && 
+    <button onClick={handleCallUser}>
+      CALL
+    </button>
+   }
+    {myStream && (
+        <>
+          <h1>My Stream</h1>
+          <ReactPlayer
+            playing
+            muted
+            height="100px"
+            width="200px"
+            url={myStream}
+          />
+        </>
+      )}
     </div>
   )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -14,14 +14,12 @@ const socketidToEmailMap = new Map();
 
 io.on('connection', (socket) => {
   console.log(`Socket Connected`, socket.id);  
-  socket.on("room:join", (data) => {
+   socket.on("room:join", (data) => {
     const { email, room } = data;
-
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit('user:joined', { email, id: socket.id })
-    socket.join(room)
-    io.to(socket.id).emit('room:join', data);
-    
-  })
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
 });
