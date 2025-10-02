14549e8e9ba87157b6983baaea3afda18520a76a
avaneesh <avaneesh.gupta5@gmail.com>
2025-10-02 18:22:39 +0530

    update socket connection URL to use production server

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:7000"), []);
+  const socket = useMemo(() => io("https://swingio.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
