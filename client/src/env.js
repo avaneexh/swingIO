d6cbc06fc70ea022b9edd4d04e8228b2ca587e85
avaneesh <avaneesh.gupta5@gmail.com>
2025-10-17 20:56:05 +0530

    Update environment configuration \

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,4 @@
 export const env = {
- BASE_URL: "http://localhost:5000",
-    // BASE_URL: "https://swingio.onrender.com",
+//  BASE_URL: "http://localhost:5000",
+    BASE_URL: "https://swingio.onrender.com",
 }
